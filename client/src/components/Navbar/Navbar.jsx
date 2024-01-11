5dfab026d910eff3bbb2121978402df9125ad377
fcoronel89 <fedec34@gmail.com>
2024-01-11 13:03:14 -0300

    add responsive styles

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,8 +3,21 @@ import { Link } from "react-router-dom";
 import Logo from "../../img/logo.png";
 import { AuthContext } from "../../context/authContext";
 import "./Navbar.scss";
-import { Box, Button, Menu, MenuItem, Fade } from "@mui/material";
+import {
+  Box,
+  Button,
+  Menu,
+  MenuItem,
+  Fade,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
 import { categories } from "../../utils/helpers";
+import MenuIcon from "@mui/icons-material/Menu";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Navbar = () => {
   const { currentUser, logout } = useContext(AuthContext);
@@ -12,6 +25,9 @@ const Navbar = () => {
 
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
+
+  const [openMenu, setOpenMenu] = useState(false);
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -66,6 +82,7 @@ const Navbar = () => {
                 component={Link}
                 to={`/?cat=${category.id}`}
                 onClick={handleClose}
+                key={category.id}
               >
                 <h6>{category.description}</h6>
               </MenuItem>
@@ -86,6 +103,48 @@ const Navbar = () => {
             </Link>
           )}
         </nav>
+        <div className="menuhamburger" onClick={() => setOpenMenu(true)}>
+          <MenuIcon />
+        </div>
+        <div className={"menu-mobile" + (openMenu ? " active" : "")}>
+          <CloseIcon
+            className="close-icon"
+            onClick={() => setOpenMenu(false)}
+          />
+          <Accordion className="accordion">
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Destinos</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              {categories.map((category) => (
+                <Link
+                  to={`/?cat=${category.id}`}
+                  onClick={() => setOpenMenu(false)}
+                  key={category.id}
+                >
+                  <h6>{category.description}</h6>
+                </Link>
+              ))}
+            </AccordionDetails>
+          </Accordion>
+          {currentUser ? (
+            <>
+              <Link className="link" to={"/write"} onClick={() => setOpenMenu(false)}>
+                Nuevo
+              </Link>
+              <span>{currentUser?.userName}</span>
+              <span onClick={logout}>Salir</span>
+            </>
+          ) : (
+            <Link className="link" to={"/login"}>
+              Entrar
+            </Link>
+          )}
+        </div>
       </Box>
     </Box>
   );
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -23,13 +23,13 @@ const Home = () => {
     <section className="home">
       <HomeHeader />
       <Box className="container posts" id="posts">
-        <Grid container columnSpacing={3} rowSpacing={3}>
+        <Grid container columnSpacing={{ xs: 0, sm: 3, md: 3 }} rowSpacing={3} columns={{ xs: 4, sm: 8, md: 12 }}>
           {isLoading && "Loading..."}
           {isError && error.message}
           {!isLoading &&
             !isError &&
             posts.map((post) => (
-              <Grid item xs={4} key={post._id}>
+              <Grid item xs={4} sm={4} md={4} key={post._id}>
                 <Box
                   className="post-card"
                   style={{ backgroundImage: `url(${post.img})` }}
